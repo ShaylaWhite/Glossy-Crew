11c8f9529cb42f292add934575ea2470696df273
ShaylaWhite <shayladwhite1@gmail.com>
2020-09-28 15:38:27 -0700

    relationships users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     has_many :reviews
-    has_many :lip_glosses, through :reviews  
+    has_many :reviewed_lip_glosses, through :reviews, source: :lip_gloss
     has_many :lip_glosses
 end
