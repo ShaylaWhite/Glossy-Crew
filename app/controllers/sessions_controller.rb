06330e02b01b7df7d4d188076e812fac3985bc6c
ShaylaWhite <shayladwhite1@gmail.com>
2020-09-28 18:03:54 -0700

    login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,5 +7,9 @@ class SessionsController < ApplicationController
     def new
     end 
 
+    def destroy
+        session.delete(:user_id)
+        redirect_to '/'
+      end
     
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
     has_many :reviewed_lip_glosses, through: :reviews, source: :lip_gloss
     has_many :lip_glosses
     has_secure_password
+
+    validates :username, uniqueness:true, presence:true
 end
