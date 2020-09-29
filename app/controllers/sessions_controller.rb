e5916d49b7001939b8e6342e16cdd2e241f63773
ShaylaWhite <shayladwhite1@gmail.com>
2020-09-28 17:25:44 -0700

    users show page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
 
-    def home
+    def welcome
 
     end 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
     has_many :reviews
     has_many :reviewed_lip_glosses, through: :reviews, source: :lip_gloss
     has_many :lip_glosses
-    has_secured_password
+    has_secure_password
 end
