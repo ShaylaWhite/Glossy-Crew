5d6126f4dcf61de8bb9c2334577a4c299255a63d
ShaylaWhite <shayladwhite1@gmail.com>
2020-09-28 20:05:38 -0700

    lip gloss controller

--- app/controllers/lip_glosses_controller.rb
+++ app/controllers/lip_glosses_controller.rb
@@ -2,6 +2,27 @@ class LipGlossesController < ApplicationController
 
  def new
     @lip_gloss = LipGloss.new 
+    @lip_gloss.build_sponsor
  end 
+
+def create
+    @lip_gloss = LipGloss.new(lip_gloss_params)
+    @lip_gloss.user_id = session[:user_id]
+        if @lip_gloss.save
+          redirect_to lip_gloss_path(@lip_gloss)
+        else
+          render :new
+     end
+end 
+
+
+
+
+ private
+ 
+ def lip_gloss_params
+    params.require(:lip_gloss).permit(:name, :price, :sponsor_id, sponsor_attributes: [:name])
+ end
+
 
 end
--- app/models/lip_gloss.rb
+++ app/models/lip_gloss.rb
@@ -3,4 +3,5 @@ class LipGloss < ApplicationRecord
   belongs_to :user #
   has_many :reviews
   has_many :users, through: :reviews 
+  accepts_nested_attributes_for :sponsor
 end
