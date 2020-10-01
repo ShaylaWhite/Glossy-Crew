ed37b2d52d115e4f6c84c723c5bb6ded75d227ce
ShaylaWhite <shayladwhite1@gmail.com>
2020-09-30 21:46:23 -0700

    review form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,14 @@
 class ReviewsController < ApplicationController
 
     def new
+        @lip_gloss = LipGloss.find_by_id(params[:lip_gloss_id])
+        @review = @lip_gloss.reviews.build
     end 
 
     def index
     end 
-
     
+
+
+
 end
