8901aa8e6578eee7f6afd3da665f4898bc0b53d8
ShaylaWhite <shayladwhite1@gmail.com>
2020-10-01 20:05:14 -0700

    gem gem 'humanize_boolean'

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,28 +1,45 @@
 class ReviewsController < ApplicationController
 
     def new
-        @lip_gloss = LipGloss.find_by_id(params[:lip_gloss_id])
-        @review = @lip_gloss.reviews.build
-    end 
-
-    def create 
-        @review = current_user.reviews.build(review_params)
-        if @review.save
-             redirect_to review_path(@review)
-        else
-            render :new
-        end
-    end 
-    
-    def show 
-      @review = Review.find_by_id(params[:id])
-    end 
-
-    def index
+        if @lip_gloss = LipGloss.find_by_id(params[:lip_gloss_id])
+            @review = @lip_gloss.reviews.build
+          else
+            @review = Review.new
+          end
     end
 
-     def review_params
-        params.require(:review).permit(:lip_gloss_id, :purchase, :comment)
-     end 
+    def create
+        @review = current_user.reviews.build(reviews_params)
+        if @review.save!
+          redirect_to review_path(@review)
+        else
+          render :new
+        end
+      end
 
+    def show
+        @review = Review.find_by_id(params[:id])
+    end
+
+    def edit
+    end
+
+    def index
+        if @lip_gloss = LipGloss.find_by_id(params[:lip_gloss_id])
+            @reviews = @lip_gloss.reviews
+        else
+            @reviews = Review.all
+        end
+    end
+    
+    private
+
+    def reviews_params
+        params.require(:review).permit(:lip_gloss_id, :purchase, :comment)
+    end
+
+    def set_review
+        @review = Review.find_by(id: params[:id])
+    end
+  
 end
