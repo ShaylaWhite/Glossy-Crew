83a345eae3885f9c432b47b111f064ee178feda9
ShaylaWhite <shayladwhite1@gmail.com>
2020-10-15 00:03:26 -0700

    omni google session methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,8 +8,15 @@ class SessionsController < ApplicationController
     end 
 
     def create
+
+        if params[:provider] == 'google_oauth2'
+            @user = User.create_by_google_omniauth(auth)
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+      
+        else 
         @user = User.find_by(username: params[:user][:username]) #find person
-        if @user && @user.authenticate(params[:user][:password]) #check password
+        @user && @user.authenticate(params[:user][:password]) #check password
             session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
@@ -27,6 +34,15 @@ class SessionsController < ApplicationController
     
 
     def omniauth
-        byebug
-    end 
+        @user = User.create_by_google_omniauth(auth)
+    
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      end
+    
+      private
+    
+        def auth
+          request.env['omniauth.auth']
+        end
 end
